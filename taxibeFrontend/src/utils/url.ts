652cbf1081e86730b1896e083c61f14839d8eb15
olivia-isaia <isaiarajhonson@gmail.com>
2025-11-19 08:19:32 +0100

    modification url

--- taxibeFrontend/src/utils/url.ts
+++ taxibeFrontend/src/utils/url.ts
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
 export const url = 'http://192.168.1.182:3000/api';
 /*export const url = 'http://localhost:3000/api';*/
-=======
-export const url = 'http://192.168.142.169:3000/api';
-
-// export const url = 'http://localhost:3000/api';
->>>>>>> 34ba022cfb8481d2bb977a970d7a3b328c52e5f5
